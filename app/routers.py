26db230d97e6874a2fe867c09fc1ba22e6e7c95c
Antonio Santana <antoniosnt.dev@gmail.com>
2026-09-18 09:24:04 -0300

    fix: adjust ApiRouter prefix parameter

--- app/routers.py
+++ app/routers.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from app.healthy.routes import router as healthy_router
 
-router = APIRouter("/api")
+router = APIRouter(prefix="/api")
 
 router.include_router(healthy_router)
 
